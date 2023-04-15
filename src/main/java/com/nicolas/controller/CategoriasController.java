e6b535ca7b9c95b6ec91ef449ac63e63f203bed5
Nicolas Golmar <nico.bocakapo09@gmail.com>
2023-04-14 23:23:21 -0300

    Agregado de funcionalidades
    Delete / Detalle /Eliminar

--- src/main/java/com/nicolas/controller/CategoriasController.java
+++ src/main/java/com/nicolas/controller/CategoriasController.java
@@ -4,8 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping(value="/categorias")
+
 public class CategoriasController {
 
 
@@ -18,7 +22,12 @@ public class CategoriasController {
 	return "categorias/formCategoria";
 	}
 	@PostMapping("/save")
-	public String guardar() {
+	public String guardar(@RequestParam("nombre") String nombre , @RequestParam("descripcion") String descripcion) {
+		
+		System.out.println("Nombre: "+nombre);
+		System.out.println("Nombre: "+descripcion);
+		
+		
 	return "categorias/listCategorias";
 	}
 	
---
+++ src/main/java/com/nicolas/controller/VacantesController.java
@@ -0,0 +1,46 @@
+package com.nicolas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/vacantes")
+public class VacantesController {
+
+	
+	
+	@GetMapping("/delete")
+	public String eliminar(@RequestParam("id") int idVacante , Model model) {
+		
+		System.out.print("Se elimino la vacante con ID " + idVacante);
+		
+		model.addAttribute("idVacante", idVacante);
+		
+		
+		return "mensaje";
+		
+	}
+	
+	
+	
+	@GetMapping("/view/{id}")
+	public String verDetalle(@PathVariable("id") int idVacante , Model model) {
+		
+		
+		System.out.println("EL ID DE LA VACANTE ES "+ idVacante);
+		
+		model.addAttribute("idVacante", idVacante);
+		
+		return "vacantes/detalleVacante";
+		
+		
+		
+	}
+	
+	
+	
+}
